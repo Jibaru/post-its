ff4149b36bea3d856d6209549c87848568399fc4
Jibaru <ignacioruedaboada@hotmail.com>
2022-02-26 13:41:04 -0500

    Added google drive service provider

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        App\Providers\GoogleDriveServiceProvider::class,
     ],
 
     /*
